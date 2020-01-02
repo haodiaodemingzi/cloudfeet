7d5c0175591a77e01b5e3ffa06ec6f4916b2a12f
root <haodiaodemingzi@gmail.com>
2020-01-02 15:35:50 +0000

    add outline api log

--- services/proxy_service/proxy_service.go
+++ services/proxy_service/proxy_service.go
@@ -11,36 +11,39 @@ import (
 	"github.com/haodiaodemingzi/cloudfeet/models"
 	"github.com/haodiaodemingzi/cloudfeet/pkg/consul"
 	log "github.com/haodiaodemingzi/cloudfeet/pkg/logging"
+	"github.com/haodiaodemingzi/cloudfeet/pkg/settings"
 )
-
 
 // 随机获取代理链接信息
 // TODO: 后期智能根据负载和网络状况返回
 // get proxy info from consul service
 func ProxyConnInfo(id string) (models.ProxyModel, error) {
 	/*
-	var model = &models.ProxyModel{}
-	proxyModel, err := model.RandomProxy()
-	if err != nil {
-		log.Error("获取ss配置失败: ", err.Error())
-	}
+		var model = &models.ProxyModel{}
+		proxyModel, err := model.RandomProxy()
+		if err != nil {
+			log.Error("获取ss配置失败: ", err.Error())
+		}
 	*/
 	var proxyModel models.ProxyModel
 	service, err := consul.GetRandomProxyService("outline-proxy")
-	if err != nil{
+	if err != nil {
 		return models.ProxyModel{}, err
 	}
-	if service == nil{
+	if service == nil {
 		return models.ProxyModel{}, errors.New("can't find service for outline-proxy")
 	}
 
 	client := httpclient.New()
-	client.SetTLSClientConfig(&tls.Config{ InsecureSkipVerify: true })
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 	// TODO: add api key from settings
-	outlineAPI := "https://" + service.Address + ":8081/TestApiPrefix/access-keys"
+	outline_api_key := settings.Config.Outline.APIKEY
+	outline_api_port := settings.Config.Outline.Port
+	outlineAPI := "https://" + service.Address + ":" + outline_api_port + "/" + outline_api_key + "/access-keys"
+	log.Info("outline api - %s", outlineAPI)
 	payload := map[string]interface{}{
-		"id": "outline-" + id,
-		"port": 10247,
+		"id":       "outline-" + id,
+		"port":     10247,
 		"password": "Divein" + id,
 	}
 	log.Info("payload json %+v", payload)
@@ -58,7 +61,7 @@ func ProxyConnInfo(id string) (models.ProxyModel, error) {
 	return proxyModel, err
 }
 
-func AddProxy(server string, port int, method string, password string) error{
+func AddProxy(server string, port int, method string, password string) error {
 	var model = &models.ProxyModel{}
 
 	model.Name = fmt.Sprintf("%s-%d", server, port)
@@ -72,9 +75,9 @@ func AddProxy(server string, port int, method string, password string) error{
 	return model.FindOrCreate(model.Domain)
 }
 
-func AddOutlineProxy() (map[string]interface{},error){
-	nodeService, err:= consul.GetRandomProxyService("outline-proxy")
-	if err != nil{
+func AddOutlineProxy() (map[string]interface{}, error) {
+	nodeService, err := consul.GetRandomProxyService("outline-proxy")
+	if err != nil {
 		log.Error(err.Error())
 		return nil, err
 	}
@@ -84,21 +87,21 @@ func AddOutlineProxy() (map[string]interface{},error){
 		nodeService.Address, nodeService.Port, apiKEY)
 	// read from setting
 	client := resty.New()
-	client.SetTLSClientConfig(&tls.Config{ InsecureSkipVerify: true })
-	body := map[string]interface{}{"username": "testuser", "password": "Diveinedu",}
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	body := map[string]interface{}{"username": "testuser", "password": "Diveinedu"}
 	resp, err := client.R().SetBody(body).Post(outlineAPI)
-	if resp != nil && resp.StatusCode() != 201{
+	if resp != nil && resp.StatusCode() != 201 {
 		return nil, errors.New("post new access-key failed")
 	}
 	return body, nil
 }
 
-func RemoveProxy(server string) error{
+func RemoveProxy(server string) error {
 	var model = &models.ProxyModel{}
 
-	where := map[string]interface{}{"server": server,}
+	where := map[string]interface{}{"server": server}
 	row, err := model.Select(where)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
